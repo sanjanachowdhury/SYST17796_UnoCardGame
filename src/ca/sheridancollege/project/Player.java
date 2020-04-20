b659e2d319867d3cbb5f1d2d0691e708a606ba58
lina-test <shalabil@sheridancollege.ca>
2020-04-20 19:47:29 -0400

    updated code

--- src/ca/sheridancollege/project/Player.java
+++ src/ca/sheridancollege/project/Player.java
@@ -2,42 +2,56 @@
  * SYST 17796 Project Winter 2019 Base code.
  * Students can modify and extend to implement their game.
  * Add your name as a modifier and the date!
+ * 
  */
 package ca.sheridancollege.project;
 
 /**
  * A class that models each Player in the game. Players have an identifier, which should be unique.
  * @author dancye, 2018
+ * audit lina shalabi, 2020.
  */
 public abstract class Player 
 {
-    private String playerID; //the unique ID for this player
+    
+    private String userName; //the unique ID for this player
     
     /**
-     * A constructor that allows you to set the player's unique ID
-     * @param name the unique ID to assign to this player.
+     * A constructor that allows you to set the player's unique name
+     * @param name the unique name to assign to this player.
      */
     public Player(String name)
     {
-        playerID= name;
+        userName= name;
+    }
+    /**
+     * a method to test the length of the user name. it should be more than 3 
+     * characters
+     * @param name
+     * @return 
+     */
+    public static boolean checkName(String name){
+        if (name.length() >= 3) {
+            return true;
+        }
+        return false;
     }
     
-    /**
-     * @return the playerID
-     */
-    public String getPlayerID() 
+    public String getuserName() 
     {
-        return playerID;
+        return userName;
     }
 
     /**
-     * Ensure that the playerID is unique
-     * @param givenID the playerID to set
+     * Ensure that the userName is unique
+     * @param givenName the userName to set
      */
-    public void setPlayerID(String givenID) 
+    public void setuserName(String givenName) 
     {
-        playerID = givenID;
+      
+        userName = givenName;
     }
+    
     
     /**
      * The method to be instantiated when you subclass the Player class
