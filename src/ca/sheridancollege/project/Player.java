f1e4c2a7f2aa29a1880bda3b3ff8d9f2c75c763e
chowdsan <sanjana.achowdhury@gmail.com>
2020-04-21 12:39:50 -0400

    refactored Player class

--- src/ca/sheridancollege/project/Player.java
+++ src/ca/sheridancollege/project/Player.java
@@ -2,61 +2,61 @@
  * SYST 17796 Project Winter 2019 Base code.
  * Students can modify and extend to implement their game.
  * Add your name as a modifier and the date!
- * 
  */
 package ca.sheridancollege.project;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * A class that models each Player in the game. Players have an identifier, which should be unique.
+ * A class that models each Player in the game. Players have an identifier,
+ * which should be unique.
+ *
  * @author dancye, 2018
- * audit lina shalabi, 2020.
  */
-public abstract class Player 
-{
-    
-    private String userName; //the unique ID for this player
-    
+public class Player {
+
+    private String playerID; //the unique ID for this player
+    List<Card> hand;
+
     /**
-     * A constructor that allows you to set the player's unique name
-     * @param name the unique name to assign to this player.
+     * A constructor that allows you to set the player's unique ID
+     *
+     * @param name the unique ID to assign to this player.
      */
-    public Player(String name)
-    {
-        userName= name;
+    public Player(String name) {
+        playerID = name;
     }
-    /**
-     * a method to test the length of the user name. it should be more than 3 
-     * characters
-     * @param name
-     * @return 
-     */
-    public static boolean checkName(String name){
-        if (name.length() >= 3) {
-            return true;
-        }
-        return false;
+
+    public Player() {
+        this.hand = new ArrayList<Card>();
     }
-    
-    public String getuserName() 
-    {
-        return userName;
+
+    public List<Card> getHandOfCards() {
+        return hand;
     }
 
     /**
-     * Ensure that the userName is unique
-     * @param givenName the userName to set
+     * @return the playerID
      */
-    public void setuserName(String givenName) 
-    {
-      
-        userName = givenName;
+    public String getPlayerID() {
+        return playerID;
     }
-    
-    
+
     /**
-     * The method to be instantiated when you subclass the Player class
-     * with your specific type of Player and filled in with logic to play your game.
+     * Ensure that the playerID is unique
+     *
+     * @param givenID the playerID to set
      */
-    public abstract void play();
-    
+    public void setPlayerID(String givenID) {
+        playerID = givenID;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{"
+                + "hand=" + hand
+                + '}';
+    }
+
 }
